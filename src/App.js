52449e351d83905bf20ed0026c7e9664141286e1
aSebastianMP <strog35777@gmail.com>
2021-03-11 20:33:21 -0600

    commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          
+        </p>
+        <a
+          className="App-link"
+          href="./ejemplo_2.html"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Practica 2
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
